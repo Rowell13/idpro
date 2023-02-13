b32c9fd31b07c1f099a5e350885aabf01a26d9f9
Eugene Kerong <rocemarycherong@yahoo.com>
2023-02-13 09:39:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php 
+    include "koneksi/koneksi.php";
+    //include "koneksi/fungsi.php";
+?>
+
+<html>
+    <?php 
+      include "template/head.php";
+    ?>
+     
+    <body>
+      
+      <?php 
+      include "template/header.php";
+      include "home.php";
+      
+      ?>
+      
+       
+        
+        
+        
+        <?php include "template/footer.php"; ?>
+
+        <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+        <script src="assets/js/main.js"></script>
+           
+        <?php //}?>
+    </body>
+
+
+</html>
